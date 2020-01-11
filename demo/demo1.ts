5eed95f759ce9db6fbbc960fb94f189121dafcb2
auth2 <auth2>
2020-01-11 17:43:51 +0800

    auth2

--- demo/demo1.ts
+++ demo/demo1.ts
@@ -2,6 +2,22 @@ document.documentElement.style['position'] = 'fixed';
 
 var ua = navigator.userAgent.toLowerCase();
 
+//auth2
+// auth2
+
+var dbName = 'dbName'
+
+const storeName = 'storeName';
+
+console.log('创建[ ' + dbName + ' / ' + storeName + ' ]成功');
+
 var userAgent = {
     ua: ua
 }
+
+const a = 1;
+var b = 2;
+const c = 3;
+let d = 4;
+
+let e = {a: a, b: b, c: c, d:d};
---
+++ demo/demo2.js
@@ -0,0 +1,5 @@
+var getImgAndWidth = function(data, dprrecord, callback) {
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext('2d');
+var img = new Image();
+}
